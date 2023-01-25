cd4f483b5f99dbd482d78a28b0f3d824e1230834
Dan Nykjær Jakobsen <dj@ropca.com>
2023-01-25 10:21:35 +0100

    Autoformatting the file before saving if lsp supports it

--- lua/config/lsp/keybinding.lua
+++ lua/config/lsp/keybinding.lua
@@ -37,6 +37,7 @@ local function keymappings(client, bufnr)
   }
   if client.server_capabilities.documentFormattingProvider then
     keymap_l.l.F = { "<cmd>lua vim.lsp.buf.format({async = true})<CR>", "Format Document" }
+    vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
   end
 
 
